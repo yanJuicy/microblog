ba5bb189d967c364e67b295636113c82555495ec
yanJuicy <hanjub3@gmail.com>
2026-01-01 01:53:13 +0900

    Web Forms

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,28 @@
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 from app import app
+from app.forms import LoginForm
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
-    user = {"username": "Miguel"}
+    user = {'username': 'Miguel'}
     posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool'
-        }
+        {'author': {'username': 'John'}, 'body': 'Beautiful day in Portland!'},
+        {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool'},
     ]
-    return render_template("index.jinja", title="Home", user=user, posts=posts)
+    return render_template('index.jinja', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for('index'))
+
+    return render_template('login.jinja', title='Sign in', form=form)
